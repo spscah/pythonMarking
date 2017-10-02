3fb2d5d7cd8ba6cc705f56232e3bb813f9626d45
Buster Parr <busterparr@icloud.com>
2017-10-02 13:32:59 +0100

    extract lines now works for funcs and classes

--- analysis/pythonModules/extractFunc.py
+++ analysis/pythonModules/extractFunc.py
@@ -16,41 +16,43 @@ def checkWhitespace(line):
 	if ord(line[0]) == 10:
 		#decimal for new line
 		return True
+	if line[0] == " ":
+		return True
 	return False
 
 
-# SOMEthing to DO WITH THE SCOPE OF I - NOT BEING UPDATED
-def extractLines(cont):
+def extractLines(cont, pat):
 	lines = [i for i in cont.split("\n") if i]
-	funcPattern = re.compile("def .+(.*):")
 	functions = []
-	#print(lines)
-
 	index = 0
 	while index < len(lines):
 		curLine = lines[index]
-		if funcPattern.match(curLine):
+		if pat.match(curLine):
 			startIndex = cont.find(curLine)
 			endIndex = -1
 			tmpIndex = index+1
-			#iterate and find the end of the func
 			while tmpIndex < len(lines):
 				if not checkWhitespace(lines[tmpIndex]):
-					subStr = lines[tmpIndex-2] + "\n" + lines[tmpIndex-1]
-					endIndex = cont.find(subStr) + len(subStr)
+					subStr = lines[tmpIndex-1]
+					#print("finding substr:\t", subStr)
+					endIndex = cont.find(subStr, startIndex) + len(subStr)
+					#print("Extracting from index ", startIndex, " to ", endIndex)
 					break
 				tmpIndex += 1
 
 			if endIndex == -1:
-				#means end of func not found, go to end of file
 				endIndex = len(cont) -1
-			#now read from startIndex to endIndex and append to functions
 			functions.append(cont[startIndex:endIndex])
 		index += 1
 	return functions
 
 
-a = extractLines(readFile("./extractFunc.py"))
-for i in a:
-	print(i)
-	print("-"*30)
+funcPattern = re.compile("def .+(.*):")
+classPattern = re.compile("class .+:")
+con = readFile("physics.py")
+a = extractLines(con, funcPattern)
+b = extractLines(con, classPattern)
+print("There are ", len(a), " functions in the file.")
+print("There are ", len(b), " classes in the file.")
+print(a)
+print(b)
